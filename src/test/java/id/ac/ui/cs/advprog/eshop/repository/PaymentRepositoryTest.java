95ca699ee113667d56fcfbb54c61935cf7d24c6b
RafaMiyauchi <rafamiyauchi@gmail.com>
2026-03-10 17:49:09 +0700

    [RED] Add tests for Payment repository.

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -0,0 +1,34 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentRepositoryTest {
+    PaymentRepository paymentRepository;
+    Payment payment;
+
+    @BeforeEach
+    void setUp() {
+        paymentRepository = new PaymentRepository();
+        Map<String, String> data = new HashMap<>();
+        data.put("voucherCode", "ESHOP1234ABC5678");
+        payment = new Payment("1", "VOUCHER", "SUCCESS", data);
+    }
+
+    @Test
+    void testSave() {
+        Payment result = paymentRepository.save(payment);
+        Payment findResult = paymentRepository.findById(payment.getId());
+        assertEquals(payment.getId(), result.getId());
+        assertEquals(payment.getId(), findResult.getId());
+    }
+
+    @Test
+    void testFindByIdNotFound() {
+        assertNull(paymentRepository.findById("non-existent-id"));
+    }
+}
