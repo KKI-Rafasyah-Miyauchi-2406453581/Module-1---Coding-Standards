0d8edc2b83779e2a8bed7e19c9d5b56e008abfd2
RafaMiyauchi <rafamiyauchi@gmail.com>
2026-03-10 19:04:33 +0700

    [REFACTOR] Move validation logic from Service to Payment model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -10,14 +10,18 @@ public class Payment {
     private String status;
     private Map<String, String> paymentData;
 
-    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+    public Payment(String id, String method, Map<String, String> paymentData) {
         this.id = id;
         this.method = method;
-        this.status = status;
-
-        if (paymentData == null || paymentData.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
         this.paymentData = paymentData;
+        this.status = "REJECTED"; // Default
+
+        if (method.equals("VOUCHER")) {
+            String code = paymentData.get("voucherCode");
+            if (code != null && code.length() == 16 && code.startsWith("ESHOP")) {
+                long digits = code.chars().filter(Character::isDigit).count();
+                if (digits == 8) this.status = "SUCCESS";
+            }
+        }
     }
 }
